b1b071d01acb244b0e582ad84fb9da390eac9a8d
vfreak <vancejones53@gmail.com>
2017-05-22 10:37:49 -0700

    AHHHHHH

--- p07470p33l3r.c
+++ p07470p33l3r.c
@@ -50,7 +50,6 @@ struct linux_dirent {
 	unsigned long d_ino;     /* Inode number */
 	unsigned long d_off;     /* Offset to next linux_dirent */
 	unsigned short d_reclen;  /* Length of this linux_dirent */
-	unsigned char d_type;
 	char d_name[256];  /* Filename (null-terminated) */
 };
 
